20312a49972dead6c291149e227d4e3abc08dda3
Harshdeep S Jawanda <hsjawanda@gmail.com>
2016-03-13 09:26:41 +0530

    Added emailing capability via SendGrid.

--- src/main/java/com/hsjawanda/gaeobjectify/util/Emailer.java
+++ src/main/java/com/hsjawanda/gaeobjectify/util/Emailer.java
@@ -5,6 +5,7 @@ package com.hsjawanda.gaeobjectify.util;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 
@@ -16,6 +17,7 @@ import java.util.logging.Logger;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.SendFailedException;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -23,6 +25,9 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 import com.google.common.base.Optional;
 
@@ -42,10 +47,10 @@ public class Emailer {
 	protected Emailer() {
 	}
 
-	public static void sendEmail(EmailData data, boolean isHtml) {
-		Properties props = new Properties();
-		log.info("Email properties: " + props);
-		Session session = Session.getDefaultInstance(props, null);
+	public static void sendEmail(EmailData data, boolean isHtml, Session session) {
+//		Properties props = new Properties();
+//		log.info("Email properties: " + props);
+//		Session session = Session.getDefaultInstance(props, null);
 		Message mesg = new MimeMessage(session);
 
 		InternetAddress from = checkNotNull(data.getFrom(), "The 'From' address can't be null");
@@ -72,6 +77,47 @@ public class Emailer {
 			log.log(Level.WARNING, "Exception while sending email.", e);
 		} catch (Exception e) {
 			log.log(Level.WARNING, "General exception catching while sending email. Stacktrace:", e);
+		}
+	}
+
+	public static void sendEmail(EmailData data, boolean isHtml) {
+		sendEmail(data, isHtml, Session.getDefaultInstance(new Properties(), null));
+	}
+
+	public static void sendGmailEmail(EmailData data, boolean isHtml) {
+		final String username="hsjawanda@gmail.com", password="nothing";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		sendEmail(data, isHtml, session);
+	}
+
+	public static void sendGridEmail(EmailData data, boolean isHtml) {
+		String username = "admin@plowns.com", password = "Admin@1234";
+		Sendgrid mail = new Sendgrid(username, password);
+		mail.setFrom(data.getFrom().getAddress()).setFromName(data.getFrom().getPersonal())
+				.setSubject(data.getSubj());
+		for (InternetAddress iaddr : data.getTo()) {
+			mail.addTo(iaddr.getAddress(), defaultString(iaddr.getPersonal()));
+		}
+		if (isHtml) {
+			mail.setText(Jsoup.clean(data.getBody(), Whitelist.none()));
+			mail.setHtml(data.getBody());
+		} else {
+			mail.setText(data.getBody());
+		}
+		try {
+			mail.send();
+		} catch (Exception e) {
+			log.log(Level.WARNING, "Error with SendGrid emailing. Stacktrace:", e);
 		}
 	}
 
